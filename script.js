5a8524c4059968c7c6493ce77512bb6348e341ce
dinfra1 <125934154+dinfra1@users.noreply.github.com>
2023-11-20 12:11:25 -0500

    Add files via upload
    Se actualiza y se modifica portafolio agregando boton de ws, boton de claro/oscuro

--- script.js
+++ script.js
@@ -1,6 +1,32 @@
 // activacion menu boton
-let menuBtn = document.querySelector('#menu_btb');
-let menuPrincipal = document.querySelector('.menu_principal');
+const menuBtn = document.querySelector('#menu_btb');
+const menuPrincipal = document.querySelector('.menu_principal');
+const $lenguaje = document.querySelector('#lenguaje');
+const body = document.querySelector('body');
+const fond = document.querySelector('.fond');
+
+fond.addEventListener('click', ()=>{
+    body.classList.toggle('active')
+})
+
+const changueLenguage = () =>{
+    let ind = $lenguaje.selectedIndex;
+    let opc = $lenguaje.options[ind];
+    let date = opc.value
+    
+       if(date === 'spanish'){
+        if(location.href === 'es/index.html'){
+            return;
+        }
+        location.href = 'es/index.html'
+       }else if(date === 'english'){
+        if(location.href === '/index.html'){
+            return;
+        }
+        location.href = '/index.html'
+       }
+}
+$lenguaje.addEventListener("change", changueLenguage)
 
 menuBtn.onclick = () =>{
     menuBtn.classList.toggle('bx-x');
@@ -28,11 +54,24 @@ ScrollReveal().reveal('.home_bio, .heading', { origin: 'top'});
 ScrollReveal().reveal('.profile_pic, .about_item, .skills_item, .project_item, .formulario', { origin: 'buttom'});
 
 
-//animacion de enunciado principal desarrollador 
-const text = new Typed ('.text_anim-developer',{
-strings: ['Edinson Ramirez','Developer Full-Stack', 'Tec in Electronics'],
-backSpeed: 100,
-typeSpeed: 100,
-backDelay: 600,
-loop: true
-});
+//animacion de enunciado principal desarrollador
+let ind = $lenguaje.selectedIndex;
+    let opc = $lenguaje.options[ind];
+    let date = opc.value
+    if(date === 'spanish'){
+        const text = new Typed ('.text_anim-developer',{
+            strings: ['Edinson Ramirez','Desarrollador Full-Stack', 'Tec en Electronica'],
+            backSpeed: 100,
+            typeSpeed: 100,
+            backDelay: 600,
+            loop: true
+            });
+    }else{
+        const text = new Typed ('.text_anim-developer',{
+            strings: ['Edinson Ramirez','Developer Full-Stack', 'Tec in Electronics'],
+            backSpeed: 100,
+            typeSpeed: 100,
+            backDelay: 600,
+            loop: true
+            });
+    }
